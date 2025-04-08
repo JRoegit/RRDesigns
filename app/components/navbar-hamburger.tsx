99c80c17418c55080b19ae26ec5a67e6420a0497
Jacob <jacobroe@rogers.com>
2025-04-08 16:37:15 -0400

    actually fixed the sidebar now

--- app/components/navbar-hamburger.tsx
+++ app/components/navbar-hamburger.tsx
@@ -7,7 +7,6 @@ import Link from "next/link"
 import { useState } from "react"
 
 export default function NavHamburger() {
-
     const [isOpen, setIsOpen] = useState(false)
     return (
         <div>
@@ -25,7 +24,7 @@ export default function NavHamburger() {
                     <BsPerson/>Contact
                 </Link>
             </div> */}
-            <div className={"fixed border-l-2 border-black/20 p-10 top-20 drop-shadow-md right-[-100%] w-fit flex flex-col gap-4 z-50 h-screen transition-all  duration-500 ease-in-out  bg-rrwhite " + (isOpen ? "" : "menuslider")}>
+            <div className={"fixed border-l-2 border-black/20 p-10 top-20 drop-shadow-md right-0 w-fit flex flex-col gap-4 z-50 h-screen transition-all  duration-500 ease-in-out  bg-rrwhite " + (isOpen ? "" : "menuslider")}>
                 <Link onClick={() => setIsOpen(!isOpen)} href={"/gallery"} className="hover:border-amber-500 font-semibold w-fit border-b-4 border-transparent text-rrcharcoal hover:cursor-pointer text-4xl transition duration-300 flex flex-row gap-2 ease-in-out">
                     <GrGallery/>Gallery
                 </Link>
